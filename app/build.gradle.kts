7432f5a3f3a6bb2710cb138656a3b8bef08a35c6
Liban Xasan <10550660+libanxasan@users.noreply.github.com>
2026-06-03 00:57:18 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,6 +139,81 @@ androidComponents {
 */
 
 dependencies {
+import com.android.build.api.dsl.LibraryExtension
+
+plugins {
+    alias(libs.plugins.kotlin.serialization)
+    id("com.android.library")
+}
+
+kotlin { jvmToolchain(25) }
+
+configure<LibraryExtension> {
+    namespace = "org.torproject.android"
+    compileSdk {
+        version = release(37)
+    }
+
+    defaultConfig {
+        minSdk = 24
+        multiDexEnabled = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        missingDimensionStrategy("free", "fullperm")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_25
+        targetCompatibility = JavaVersion.VERSION_25
+    }
+
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+
+    testOptions { execution = "ANDROIDX_TEST_ORCHESTRATOR" }
+
+    buildTypes {
+        getByName("release") {
+            isShrinkResources = false
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.txt"
+            )
+        }
+        getByName("debug") {
+        }
+    }
+
+    ndkVersion = "29.0.14206865"
+    externalNativeBuild {
+        ndkBuild {
+            path = file("src/main/jni/Android.mk")
+        }
+    }
+
+    packaging {
+        resources {
+            excludes += listOf("META-INF/androidx.localbroadcastmanager_localbroadcastmanager.version")
+        }
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+        disable += "InvalidPackage"
+        htmlReport = true
+        lintConfig = file("../lint.xml")
+        textReport = false
+        xmlReport = false
+    }
+
+}
+
+dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.preference)
@@ -155,18 +230,11 @@ dependencies {
     implementation(libs.upnp)
     implementation(libs.quickie)
 
-    // IPtProxy (for Snowflake, obfs4, dnstt and all other pluggable transports)
     implementation(libs.iptproxy)
-    // uncomment to use a local build of IPtProxy:
-    // implementation(files("../../IPtProxy/IPtProxy.aar"))
 
-
-    // Tor
     implementation(files("../libs/geoip.jar"))
     api(libs.guardian.jtorctl)
     api(libs.tor.android)
-    // uncomment to use a local build of tor-android:
-    // api(files("../../tor-android/tor-android-binary/build/outputs/aar/tor-android-binary-debug.aar"))
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso)
@@ -181,12 +249,6 @@ afterEvaluate {
     tasks.named("preBuild") {
         dependsOn(copyLicenseToAssets)
     }
-    /*
-    tasks.named { it == "mergeNightlyDebugAssets" || it == "mergeFullpermDebugAssets" }
-        .configureEach {
-            mustRunAfter(updateBuiltinBridges)
-        }
-    */
 }
 
 val copyLicenseToAssets by tasks.registering(Copy::class) {
